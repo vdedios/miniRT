d3dc3fe6e80fcd79763f45dc814a34dd1e6e3e36
Victor <victor.dedios@outlook.com>
2020-06-12 23:37:46 +0200

    fix shadow with non paralell vectors

--- src/shadows/shadows.c
+++ src/shadows/shadows.c
@@ -33,12 +33,17 @@ int             ft_between_light_source(t_vector light, t_vector point, t_vector
     t_vector    light_ray;
     double      mod_global;
     double      mod_shadow;
+    double      angle;
 
     shadow_ray = ft_sub_vector(point, origin);
     light_ray = ft_sub_vector(light, origin);
     mod_shadow = ft_mod_vector(shadow_ray);
     mod_global = ft_mod_vector(light_ray);
-    if (mod_shadow > 0.0001 && mod_shadow < mod_global)
+    angle = ft_dot_product(shadow_ray, light_ray)
+        /(mod_global * mod_shadow);
+    if (mod_shadow > 0.0001 && mod_shadow < mod_global
+        && (angle < (1 + FLT_EPSILON)
+        && angle > (1 - FLT_EPSILON)))
         return (1);
     return (0);
 }
